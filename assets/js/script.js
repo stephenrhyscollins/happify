43442a809c87660c8d8dd3254f71140770b38633
Stephen Rhys Collins <stephenrhyscollins@me.com>
2018-04-15 02:22:01 +0100

    hotfix

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,6 +67,7 @@ $(document).ready(function(){
 
   	$('#nav').click(loadPartial);
     $('#title').click(toggleMenu);
+    setupLinks();
 
 
 
@@ -98,7 +99,21 @@ function resize() {
       fullscreen = !fullscreen;
     });*/
 
-
+function setupLinks(){
+  if(("standalone" in window.navigator) && window.navigator.standalone){
+    var noddy, remotes = false;
+    document.addEventListener('click', function(event) {
+      noddy = event.target;
+      while(noddy.nodeName !== "A" && noddy.nodeName !== "HTML") {
+        noddy = noddy.parentNode;
+      }
+      if('href' in noddy && noddy.href.indexOf('http') !== -1 && (noddy.href.indexOf(document.location.host) !== -1 || remotes)){
+        event.preventDefault();
+        document.location.href = noddy.href;
+      }
+    },false);
+  }
+}
 
 
 
